6d93450859e9bf2a44317d094277ccf27fa1935b
Diego Rondão <drondao@autodoc.com.br>
2020-03-18 11:55:27 -0300

    Update Project

--- resources/views/dashboard/adverts/index.blade.php
+++
@@ -1,1 +0,0 @@
-<h1>AQUI Anúncios</h1>
--- routes/web.php
+++ routes/web.php
@@ -1,25 +1,16 @@
 <?php
 
-Route::get('/', 'Store\StoreController@index')->name('store-index');
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
 
-Route::prefix('/auth')->group(function () {
-     Route::get('/login', 'Login\LoginController@index')->name('login-index');
-     Route::post('/check', 'Login\LoginController@validateLogin')->name('login-validate');
-     Route::get('/reset', 'Login\LoginController@reset')->name('login-pass-reset');
+Route::get('/', function () {
+    return view('welcome');
 });
-
-
-Route::prefix('/dashboard')->group(function () {
-    Route::get('/', 'Dashboard\DashboardController@index')->name('dashboard-index');    
-
-	Route::prefix('/produto')->group(function () {
-	    Route::get('/delete', 'Dashboard\ProdutoController@delete')->name('produto-delete');    
-	});
-
-	Route::prefix('/user-profile')->group(function () {
-	    Route::get('/', 'Dashboard\UserController@index')->name('user-profile');    
-	});
-});	
-
-//diego.developer
-//mGTURz$*BNUozfycZb4V
